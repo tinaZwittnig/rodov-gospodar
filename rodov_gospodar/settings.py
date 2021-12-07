5b0e9f283a2afbb16e12732a53dc09327df073e7
tinaZwittnig <tina.zwittnig@gmail.com>
2021-12-07 22:58:48 +0100

    secret_key

--- rodov_gospodar/settings.py
+++ rodov_gospodar/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ${{secret.DJANGO_SECRET_KEY}}
+SECRET_KEY = 'django-insecure-ub6ktu9_nofn&s2a0&a5njot@tut&htiv*%s-b+jchm!w33wxm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
