c45a03b7264bdb113b685fbf7d0048ac3a548116
tinaZwittnig <tina.zwittnig@gmail.com>
2022-01-28 01:18:48 +0100

    admin changed

--- izposoja/admin.py
+++ izposoja/admin.py
@@ -1,6 +1,76 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.admin import UserAdmin as UserAdmin2
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+
 from .models import *
-admin.site.register(Oseba)
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    class Meta:
+        model = Oseba
+        fields = ('email', 'telefonska_stevilka', 'ime', 'priimek')
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+    class Meta:
+        model = Oseba
+        fields = ('email', 'telefonska_stevilka', 'ime', 'priimek', 'is_active', 'is_admin')
+
+
+class UserAdmin(UserAdmin2):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'telefonska_stevilka', 'ime', 'priimek', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('telefonska_stevilka', 'ime', 'priimek',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'telefonska_stevilka', 'ime', 'priimek', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+# Now register the new UserAdmin...
+admin.site.register(Oseba, UserAdmin)
 admin.site.register(Omara)
 admin.site.register(Lokacija)
+admin.site.register(Oprema)
 # Register your models here.
